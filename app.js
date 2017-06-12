33e7d2b9bd83b63faf2d63c3cd2a197026baeda9
DH-TB <929659475@qq.com>
2017-06-12 20:45:04 +0800

    form post and ajax post

---
+++ app.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let app =express();
+let path = require('path');
+let multer = require('multer');
+
+// app.use(express.static('./'));
+app.use('/ajax',express.static('./ajax.html'));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve('upload'));
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+
+const upload = multer({storage: storage});
+
+app.post('/upload', upload.single('logo'), function(req, res, next) {
+    res.send({
+        err: null,
+        filePath: 'upload/' + path.basename(req.file.path)
+    });
+});
+
+app.post('/uploadImg', upload.single('avatar'), function(req, res, next) {
+    res.send({
+        err: null,
+        filePath: 'upload/' + path.basename(req.file.path)
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log('listing 3000');
+});
